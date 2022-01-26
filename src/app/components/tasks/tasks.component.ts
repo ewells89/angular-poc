74222a01d05214dd15e1f40d57151fe2c69c9886
Elyse Wells <cewells89@gmail.com>
2022-01-26 11:09:51 -0500

    generated task component. assigned component property to component class.

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Task} from '../../Task';
+  // this is importing the interface for tasks. this is because whenever a task is defined we want to use the interface.
+import {TASKS} from '../../mock-tasks';
+  // this brings in the mock json data for the tasks. this would normally be data from the backend.
+
+
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
+  tasks: Task[] = TASKS
+  // this is assigning the tasks as a property of the component.
 
   constructor() { }
 
