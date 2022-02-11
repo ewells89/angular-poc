2cdc883009bfe88813c2aa6f3589ff3e066c6227
Elyse Wells <cewells89@gmail.com>
2022-02-11 11:32:39 -0500

    added toggle function to change add button in header component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-header',
@@ -7,13 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   title: string = 'Task Tracker';
+  showAddTask:boolean;
+  subscription: Subscription;
 
-  constructor() { }
+  constructor( private uiService:UiService) { 
+  // to use a service it must be added to the constructor.
+
+  // Add button - to hide/show the add component, once toggleAddTask() in the service is fired,
+  // the subscription must be used to watch for that event.
+  // setting up a subscription for the toggle:
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe(value => this.showAddTask = value)
+  }
+
 
   ngOnInit() {
   }
 
   toggleAddTask() {
-    console.log('toggle');
+    this.uiService.toggleAddTask(); 
+  
+    // console.log('toggle');
   }
+
+
 }
